68d61cd39fa573a6ab83a014b29be578881ff1ac
angel david marin bedoya <angeldavidmarinbedoya71@gmail.com>
2026-08-21 03:46:48 -0500

    se agrego menu hamburguesa

---
+++ code/js/main.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#open_menu");
+const cerrar = document.querySelector("#close_menu");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () =>{
+    nav.classList.remove("visible");
+})
