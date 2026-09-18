96e0ed6a51593f94bc6bc9829effb72e4b24ac5e
angel david marin bedoya <angeldavidmarinbedoya71@gmail.com>
2026-09-18 00:29:54 -0500

    funcionalidades con el js, se empezo a simular los roles, y actualmente se esta mejorando el uso de estas mismas

--- code/js/form.js
+++ code/js/form.js
@@ -1,11 +1,58 @@
 const container = document.querySelector(".container");
+
+const buttonSingUpFrom = document.querySelector("#btn-sign-up-from");
+const buttonSingInFrom = document.querySelector("#btn-sign-in-from");
+
+const validateSingIn = document.querySelectorAll(".value-sign-in");
+const validateSingUp = document.querySelectorAll(".value-sign-up");
+
 const btnSingUp = document.getElementById("btn-sign-up");
 const btnSingIn = document.getElementById("btn-sign-in");
 
 btnSingIn.addEventListener("click",()=>{
-    container.classList.remove("toggle");
+    container.classList.remove("toggle")
+    validateSingIn.forEach(function(field){
+        field.classList.remove("errorField");
+    })
 })
 
 btnSingUp.addEventListener("click",()=>{
-    container.classList.add("toggle");
+    container.classList.add("toggle")
+    validateSingUp.forEach(function(field){
+        field.classList.remove("errorField");
+    })
+    
 })
+
+buttonSingInFrom.addEventListener("click", (event)=>{
+    event.preventDefault()
+    validateSingIn.forEach((field)=>{
+        if (field.value == ""){
+            field.classList.add("errorField");
+        }
+    })
+})
+
+buttonSingUpFrom.addEventListener("click", (event)=>{
+    event.preventDefault()
+        validateSingUp.forEach(function(field){
+        if (field.value == ""){
+            field.classList.add("errorField");
+        }
+    })
+})
+
+validateSingIn.forEach(function(field){
+    field.addEventListener("keyup", function(){
+    field.classList.remove("errorField");
+    })
+})
+
+validateSingUp.forEach(function(field){
+    field.addEventListener("keyup", function(){
+    field.classList.remove("errorField");
+    })
+})
+
+
+
